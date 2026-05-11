141b8221dd81227586f057208d1999fdf86db143
HJinS <windowed@naver.com>
2026-05-11 11:58:51 +0900

    project init: 프로젝트 구조 정립

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 	kotlin("plugin.jpa") version "2.2.21"
 }
 
-group = "warmup"
+group = "io.github.jin.warmup"
 version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(25)
+		languageVersion = JavaLanguageVersion.of(24)
 	}
 }
 
